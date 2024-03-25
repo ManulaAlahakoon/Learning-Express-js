0aa39ab83fa3ff93b08e09c4f4e1bddd814d647b
Manula Alahakoon <manulaalahakoon@gmail.com>
2024-03-25 11:05:17 +0530

    Making and using middleware

--- index.js
+++ index.js
@@ -3,8 +3,18 @@ const app = express()
 const user = require('./routers/user')
 const student = require('./routers/student')
 
-app.use('/api/users',user)
+app.use('/api/users',username,password,user)
 app.use('/api/students', student)
+
+function username(req, res, next) {
+    console.log("Username is correct - Username middleware worked correctly")
+    next()
+}
+
+function password(req, res, next) {
+    console.log("Password is corrrect - Password middleware worked correctly")
+    next()
+}
 
 console.log(process.env.PORT)
 
