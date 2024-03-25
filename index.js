10473baeaed187eaa5e30d6e3b2253426fd5d2e5
Manula Alahakoon <manulaalahakoon@gmail.com>
2024-03-25 14:20:22 +0530

    Using third party middleware - Morgan

--- index.js
+++ index.js
@@ -1,10 +1,12 @@
 const express = require('express')
 const app = express()
+const morgan = require('morgan')
 const user = require('./routers/user')
 const student = require('./routers/student')
 const teacher = require('./routers/teacher')
 
 app.use(express.json())
+app.use(morgan('dev'))
 app.use('/api/users',username,password,user)
 app.use('/api/students', addIdToReq, addUsernameToReq, student)
 app.use('/api/teachers',teacher)
